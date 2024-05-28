fc2dd19eb903bc52ab2c0d8c9e49bb9e122e9e01
CoderKaranRavi <karanravitvr@gmail.com>
2024-05-29 02:37:53 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//using the package through getting in require
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const express = require("express");
+const fs = require("fs");
+const moment = require("moment");
+const path = require("path");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = express();
+
+//get used to display defaulty when the api hit on the sever
+app.get("/", (req, res) => {
+
+    try {
+        //condition to create a file with content - current date and time
+        fs.writeFileSync("date-time.txt", `Current date and time  is  ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+        res.send("File created successfully!");
+
+    } catch (error) {
+        console.log("Error : ", error);
+        res.status(500).send("Error creating file");
+    }
+    const dirData = fs.readdirSync(path.dirname(__filename));
+    dirData.forEach((files)=>{
+        console.log(dirData);
+    })
+  
+
+
+})
+
+
+app.listen(3001);
